3572971a248ded58f5eeb717958b5764627b336c
tamubun <sutebubon@gmail.com>
2014-10-16 23:54:52 +0900

    Audioletは音割れまくりで低音高音出せないので止めた
    まだ、音の対応無茶苦茶

--- computer/pchord/pchord.js
+++ computer/pchord/pchord.js
@@ -1,50 +1,37 @@
-function playExample() {
-    var Synth = function(audiolet, frequency) {
-        AudioletGroup.call(this, audiolet, 0, 1);
-        // Basic wave
-        this.saw = new Saw(audiolet, frequency);
+'use strict'
 
-        // Gain envelope
-        this.gain = new Gain(audiolet);
-        this.env = new PercussiveEnvelope(audiolet, 1, 0.1, 0.1,
-            function() {
-                this.audiolet.scheduler.addRelative(0, this.remove.bind(this));
-            }.bind(this)
-        );
-        this.envMulAdd = new MulAdd(audiolet, 0.3, 0);
+$(function() {
+  var context = new AudioContext(),
+      req, buffer;
 
-        // Main signal path
-        this.saw.connect(this.gain);
-        this.gain.connect(this.outputs[0]);
+  req = new XMLHttpRequest();
+  req.open('GET', 'scale.mp3', true);
+  req.responseType = 'arraybuffer';
+  req.onload = function() {
+    context.decodeAudioData(req.response, function(b) {
+      buffer = b;
+      $('button').click(function() { play(+$(this).attr('num'));})
+    });
+  };
+  req.send();
 
-        // Envelope
-        this.env.connect(this.envMulAdd);
-        this.envMulAdd.connect(this.gain, 0, 1);
-    };
-    extend(Synth, AudioletGroup);
+  function play(n) {
+    if ( n < 2 ) {
+      var src = context.createBufferSource();
 
-    var SchedulerApp = function() {
-        this.audiolet = new Audiolet();
+      src.buffer = buffer;
+      src.connect(context.destination);
+      src.start(0, 30+n*2+0.05, 0.9);
+    } else {
+      var src1 = context.createBufferSource(),
+          src2 = context.createBufferSource();
 
-        this.scale = new MajorScale();
-
-        // I IV V progression
-        var chordPattern = new PSequence([[0, 2, 4],
-                                          [3, 5, 7],
-                                          [4, 6, 8]]);
-        // Play the progression
-        this.audiolet.scheduler.play([chordPattern], 1,
-                                     this.playChord.bind(this));
-    };
-
-    SchedulerApp.prototype.playChord = function(chord) {
-        for (var i = 0; i < chord.length; i++) {
-            var degree = chord[i];
-            var frequency = this.scale.getFrequency(degree, 16.352, 4);
-            var synth = new Synth(this.audiolet, frequency);
-            synth.connect(this.audiolet.output);
-        }
-    };
-
-    var app = new SchedulerApp();
-};
+      src1.buffer = buffer;
+      src1.connect(context.destination);
+      src2.buffer = buffer;
+      src2.connect(context.destination);
+      src1.start(0, 30+0.05, 0.9);
+      src2.start(0, 30+2.05, 0.9);
+    }
+  }
+});
